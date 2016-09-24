60552c07fa91cbe89c381a90802a59e61d309b42
Jeffrey Effendy <wawa_union.zone@live.com>
2016-09-25 01:26:40 +0800

    Add UVA11683 Laser Sculpture

---
+++ UVA/UVA11683.cpp
@@ -0,0 +1,29 @@
+#include "C.h"
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	Test *t = new Test("UVA11683");
+	t->begin();
+	int W, H;
+	while (cin >> H >> W && H != 0)
+	{
+		int prevHeight = 0;
+		int res = 0;
+		for (int i = 0; i < W; i++)
+		{
+			int x;
+			cin >> x;
+			int height = H - x;
+			res += max(0, height - prevHeight);
+			prevHeight = height;
+			
+		}
+		cout << res << endl;
+	}
+	delete t;
+	return 0;
+}
